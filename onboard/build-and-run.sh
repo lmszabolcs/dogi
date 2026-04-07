a4dca0930dfce1958778559acd1dc6413a74f60d
CserDani <cserhegyidaniel@gmail.com>
2026-04-07 20:08:43 +0200

    Added build+run script for "onboard" component in its directory

---
+++ onboard/build-and-run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Builds the onboard docker image (named "onboard") 
+# and runs the container (also named "onboard") with 
+# the specified IP address as an environment variable.
+
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Valid IP format for parameter running with IP: $1"
+else
+    echo "Invalid IP format for parameter: '$1' (Please provide X.X.X.X address)"
+    exit 1
+fi
+
+# Build docker image in the current directory
+docker build -t onboard .
+
+# Run the docker container with specified IP address
+docker run \
+    --name onboard \
+    --device /dev/video0:/dev/video4 \
+    --device /dev/serial0:/dev/serial0 \
+    --network host \
+    -e VIDEO_DST=$1 \
+    -e VIDEO_PORT=5100 \
+    onboard
